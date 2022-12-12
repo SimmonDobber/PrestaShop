5207030ff24d983867d38e2a99c760a7b95fc990
paulina <zosiasamosia209@gmail.com>
2022-12-12 15:33:10 +0100

    poprawki

---
+++ selenium-tests-ostateczne/README.md
@@ -0,0 +1,6 @@
+
+how to run (from selenium-tests-ostateczne):
+
+    npm install
+
+    npm run test
--- var/cache/prod/smarty/compile/7a/28/12/7a281234cde2ef4a8662856d900eabdb25a4f379_0.file.preload.tpl.php
+++ var/cache/prod/smarty/compile/7a/28/12/7a281234cde2ef4a8662856d900eabdb25a4f379_0.file.preload.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-12-11 21:04:06
+/* Smarty version 4.2.1, created on 2022-12-12 15:12:44
   from '/var/www/html/admin-dev/themes/default/public/preload.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_639637b6b77060_33237670',
+  'unifunc' => 'content_639736dcd72657_81767452',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7a281234cde2ef4a8662856d900eabdb25a4f379' => 
     array (
       0 => '/var/www/html/admin-dev/themes/default/public/preload.tpl',
-      1 => 1670776550,
+      1 => 1670854210,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_639637b6b77060_33237670 (Smarty_Internal_Template $_smarty_tpl) {
+function content_639736dcd72657_81767452 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link
       rel="preload"
       href="<?php echo $_smarty_tpl->tpl_vars['admin_dir']->value;?>
