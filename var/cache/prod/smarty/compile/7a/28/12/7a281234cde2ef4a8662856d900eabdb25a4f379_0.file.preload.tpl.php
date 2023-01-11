f033aa6469b7fcbe57527218114335de28f72903
Pyerdowah <zosiasamosia209@gmail.com>
2023-01-11 15:09:08 +0100

    photos1

--- var/cache/prod/smarty/compile/7a/28/12/7a281234cde2ef4a8662856d900eabdb25a4f379_0.file.preload.tpl.php
+++ var/cache/prod/smarty/compile/7a/28/12/7a281234cde2ef4a8662856d900eabdb25a4f379_0.file.preload.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-12-12 15:12:44
+/* Smarty version 4.2.1, created on 2023-01-11 14:23:04
   from '/var/www/html/admin-dev/themes/default/public/preload.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_639736dcd72657_81767452',
+  'unifunc' => 'content_63beb838952453_00690225',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7a281234cde2ef4a8662856d900eabdb25a4f379' => 
     array (
       0 => '/var/www/html/admin-dev/themes/default/public/preload.tpl',
-      1 => 1670854210,
+      1 => 1673443147,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_639736dcd72657_81767452 (Smarty_Internal_Template $_smarty_tpl) {
+function content_63beb838952453_00690225 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link
       rel="preload"
       href="<?php echo $_smarty_tpl->tpl_vars['admin_dir']->value;?>
